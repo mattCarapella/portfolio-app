661a78d3d2078f46929ce6c7d076ce72dfbe284a
Backyardcrypto <alex.scagz@backyardcrypto.com>
2019-09-30 11:01:59 -0400

    add resume page

--- pages/resume.js
+++ pages/resume.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 import BasePage from '../components/BasePage';
 import BaseLayout from '../components/layouts/BaseLayout';
+import {Row, Col} from 'reactstrap';
 
 class Resume extends Component {
 	render() {
 		return (
 			<BaseLayout {...this.props.auth}>
 				<BasePage className='resume-page'>	
-					<h1>Resume Page</h1>
+					<Row>
+						<Col md={{size: 8, offset: 2}}>
+							<iframe style={{width: '100%', height: '800px'}} src='/static/resume.pdf'></iframe>
+
+							<div className='resume-title'>
+								<a download='resume.pdf' className='btn btn-success' href='/static/resume.pdf'>
+									Download
+								</a>
+							</div>
+							
+						</Col>
+					</Row>
 				</BasePage>
 			</BaseLayout>
 		);
