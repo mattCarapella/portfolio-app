0e52c06ef829acd21b958600eb3acd4c4076eb6c
Backyardcrypto <alex.scagz@backyardcrypto.com>
2019-10-01 11:23:32 -0400

    performance fix 1

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,3 +1,8 @@
+// module.exports = {
+// 	DB_URI: process.env.DB_URI
+// }
+
+
 module.exports = {
-	DB_URI: process.env.DB_URI
+	DB_URI: 'mongodb+srv://admin:admin999@cluster0-dwfvm.mongodb.net/test?retryWrites=true&w=majority'
 }
